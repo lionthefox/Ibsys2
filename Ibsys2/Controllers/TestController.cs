c879ddd53aa47da4c0623260be94db26ee428caa
lionthefox <leon.fuchs@planetfuchs.de>
2020-03-24 09:51:36 +0100

    init

---
+++ Ibsys2/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+
+namespace Ibsys2.Controllers
+{
+  [ApiController]
+  [Route("test")]
+  public class TestController : ControllerBase
+  {
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet("get")]
+    public object Get()
+    {
+      return new
+      {
+        Name = "Test",
+        Author = "Leon",
+        Date = DateAndTime.Now
+      };
+    }
+  }
+}
