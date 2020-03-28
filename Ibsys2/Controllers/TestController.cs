6cd3d80a9e5c33da84141d2aff292dbcc0922aeb
lionthefox <leon.fuchs@planetfuchs.de>
2020-03-28 19:11:25 +0100

    -added results.xsd and .cs generator script -added XmlParser

--- Ibsys2/Controllers/TestController.cs
+++ Ibsys2/Controllers/TestController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.VisualBasic;
 
 namespace Ibsys2.Controllers
 {
@@ -8,22 +11,33 @@ namespace Ibsys2.Controllers
   [Route("test")]
   public class TestController : ControllerBase
   {
-    private readonly ILogger<TestController> _logger;
+    private readonly ILogger<TestController> logger;
 
     public TestController(ILogger<TestController> logger)
     {
-      _logger = logger;
+      this.logger = logger;
     }
 
     [HttpGet("get")]
     public object Get()
     {
-      return new
+      return JsonSerializer.Serialize(ParseResultsXml());
+    }
+
+    private results ParseResultsXml()
+    {
+      try
       {
-        Name = "Test",
-        Author = "Leon",
-        Date = DateAndTime.Now
-      };
+        using var fileStream = new FileStream(@".\Data\resultServlet.xml", FileMode.Open);
+        var xmlSerializer = new XmlSerializer(typeof(results));
+        var results = xmlSerializer.Deserialize(fileStream) as results;
+        return results;
+      }
+      catch (Exception exception)
+      {
+        logger.LogError(exception, exception.Message);
+        return null;
+      }
     }
   }
 }
---
+++ Ibsys2/Models/XSD/GenerateClassesFromXSD.ps1
@@ -0,0 +1,1 @@
+.\xsd.exe /c .\results.xsd
