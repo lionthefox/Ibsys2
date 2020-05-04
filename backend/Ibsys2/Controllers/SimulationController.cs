76b58e5a9b4f1032e9def71cf058179c5442b89e
lionthefox <leon.fuchs@planetfuchs.de>
2020-05-04 22:03:18 +0200

    Accept xml

--- backend/Ibsys2/Controllers/SimulationController.cs
+++ backend/Ibsys2/Controllers/SimulationController.cs
@@ -6,42 +6,42 @@ using Microsoft.Extensions.Logging;
 
 namespace Ibsys2.Controllers
 {
-    [ApiController]
-    [Route("simulation")]
-    public class SimulationController : ControllerBase
+  [ApiController]
+  [Route("simulation")]
+  public class SimulationController : ControllerBase
+  {
+    private readonly ILogger<SimulationController> _logger;
+    private readonly SimulationService _simulationService;
+
+    public SimulationController(
+      ILogger<SimulationController> logger,
+      SimulationService simulationService)
     {
-        private readonly ILogger<SimulationController> _logger;
-        private readonly SimulationService _simulationService;
-
-        public SimulationController(
-          ILogger<SimulationController> logger,
-          SimulationService simulationService)
-        {
-            _logger = logger;
-            _simulationService = simulationService;
-        }
-
-        [HttpGet]
-        public string Info()
-        {
-            return
-                "To start the simulation send a POST request, with the last period results as json body, to this URL.";
-        }
-
-        [HttpPost]
-        public results Simulate([FromBody] SimulationInput input)
-        {
-            try
-            {
-                _simulationService.Start(input.Results);
-                _logger.LogInformation("Simulation successful");
-                return _simulationService.LastPeriodResults;
-            }
-            catch (Exception exception)
-            {
-                _logger.LogError("Simulation failed:", exception);
-                return null;
-            }
-        }
+      _logger = logger;
+      _simulationService = simulationService;
     }
+
+    [HttpGet]
+    public string Info()
+    {
+      return
+        "To start the simulation send a POST request, with the last period results as json body, to this URL.";
+    }
+
+    [HttpPost]
+    public results Simulate([FromBody] results input)
+    {
+      try
+      {
+        _simulationService.Start(input);
+        _logger.LogInformation("Simulation successful");
+        return _simulationService.LastPeriodResults;
+      }
+      catch (Exception exception)
+      {
+        _logger.LogError("Simulation failed:", exception);
+        return null;
+      }
+    }
+  }
 }
